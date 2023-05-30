dff4b8fb3c78a00c94c9b1d0ec5746e1ac1ecae9
Your Name <martin.lakatos21@bathspa.ac.uk>
2023-05-30 01:08:11 +0100

    finito

--- pages/index.js
+++ pages/index.js
@@ -39,9 +39,35 @@ function createModal(image) {
   modalImg.classList.add("modal-content");
   modalImg.id = "img01";
   modalImg.src = image.src;
+  modalImg.dataset.fullSrc = image.dataset.fullSrc; // Store the full-resolution image URL
 
   modal.appendChild(modalImg);
   document.body.appendChild(modal);
+
+  // Add event listener to close the modal when clicking outside the image
+  modal.addEventListener("click", (event) => {
+    if (event.target === modal) {
+      modal.style.display = "none";
+    }
+  });
+
+  // Function to handle image click event
+  function openModalImage(event) {
+    const img = event.target;
+    const fullSrc = img.dataset.fullSrc;
+
+    // Create a new image element for the full-resolution version
+    const fullImg = new Image();
+    fullImg.src = fullSrc;
+    fullImg.onload = function () {
+      // Replace the lower-quality image with the full-resolution version
+      img.src = fullImg.src;
+      img.classList.add("zoom-in"); // Add a class for the zoom effect
+    };
+  }
+
+  // Add event listener to the modal image
+  modalImg.addEventListener("click", openModalImage);
 
   return modal;
 }
